8df1a8185c489b2c04e83184c6bfded2d18198ae
casper-van-der-krift <73126397+casper-van-der-krift@users.noreply.github.com>
2023-12-09 15:11:38 +0100

    Add files via upload

---
+++ machine.py
@@ -0,0 +1,31 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "coffee": 24,
+            "milk": 100,
+        },
+        "cost": 2.50,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "coffee": 24,
+            "milk": 100,
+        },
+        "cost": 3.00,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
